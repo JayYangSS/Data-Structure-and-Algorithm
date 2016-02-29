f02ba1c7049f71011e5dd105c94450b4c217bff6
JayYangSS <694728662@qq.com>
2016-02-29 10:35:01 +0800

    judging wether two trees have same part

--- tree/Tree.cpp
+++ tree/Tree.cpp
@@ -1,6 +1,33 @@
 #include"Tree.h"
 #include<queue>
 
+
+//judge is the two trees has the same root node and the same part
+bool hasTree(Node* p1,Node* p2){
+	if (p2==NULL)return true;
+	if(p1==NULL)return false;
+
+	if(p1->val!=p2->val)return false;
+
+	bool result=false;
+	result=hasTree(p1->lchild,p2->lchild)&&hasTree(p1->rchild,p2->rchild);
+	return result;
+}
+
+
+//judge if the two trees has the same part
+bool isHaveSubTree(Node* p1,Node* p2){
+	if(p2==NULL)return true;
+	if(p1==NULL)return false;
+	bool result=false;
+	result=hasTree(p1,p2);
+	if(!result)
+		result=isHaveSubTree(p1->lchild,p2->lchild);
+	if(!result)
+		result=isHaveSubTree(p1->rchild,p2->rchild);
+	return result;
+}
+
 Node* Tree::create(Node* head){
 	
 	int input;
@@ -83,5 +110,14 @@ int main(int argc, char const *argv[])
 	//inOrder search
 	cout<<"postOrder:"<<endl;
 	t.postOrder(node);
+
+	//test the function isHaveSubTree
+	Tree t2;
+	Node *node2;
+	node2=t2.create(node2);
+	if(isHaveSubTree(node,node2))
+		cout<<"the two trees have same part"<<endl;
+	else
+		cout<<"the two trees don't have same part"<<endl;
 	return 0;
 }
