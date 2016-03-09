c10cfdc3b0999426f720026ff8fd1b4b15981c23
JayYangSS <694728662@qq.com>
2016-03-09 23:19:11 +0800

    find the greatest common divisor

---
+++ other/gcd.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+//find the greatest common divisor
+int gcd(int a,int b){
+
+	int big=a>b?a:b;
+	int small=a>=b?b:a;
+	if(big%small==0)return small;
+	int result=gcd(big,big%small);
+	return result;
+}
+
+//find the least common multiple
+int lcm(int a,int b){
+	int temp;
+	temp=gcd(a,b);
+	return a*b/temp;
+}
+
+
+int main(){
+	int a=0,b=0;
+	cin>>a;
+	cin>>b;
+	cout<<"the greatest common divisor is:"<<gcd(a,b)<<endl;
+	cout<<"the least common multiple is:"<<lcm(a,b)<<endl;
+}
